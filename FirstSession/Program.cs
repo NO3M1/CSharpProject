3474f95d05e0a9abb9736e31070aff46397ab811
Noemi <szokenoemi@yahoo.com>
2023-03-08 09:49:43 +0200

    Create Program.cs

---
+++ FirstSession/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int BoxA, BoxB, BoxC;
+BoxA = 34;
+BoxB = 4;
+
+BoxC =BoxA + BoxB; // 38
+BoxC = BoxA - BoxB; //30
+BoxC = BoxA * BoxB; //136
+BoxC = BoxA % BoxB; //2 modulo division
+BoxC = BoxA / BoxB; // 8 bc we working with int . with double should be 8.5
+
+
+Console.WriteLine(BoxC);
+
+//Console.WriteLine("Yeeeee, I have the chance to learn C#");
+//Console.ReadLine();
